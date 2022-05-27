aab6e93b2a8d7ac21e2deeccbf2fd31b60a30fa7
Ian Hundere <138915+ianhundere@users.noreply.github.com>
2022-05-27 11:38:42 -0400

    Adds sleep to iptables.sh.

--- jackett/iptables.sh
+++ jackett/iptables.sh
@@ -179,11 +179,11 @@ iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 # accept output from local loopback adapter
 iptables -A OUTPUT -o lo -j ACCEPT
 
+sleep 120
+
 echo "[INFO] iptables defined as follows..." | ts '%Y-%m-%d %H:%M:%.S'
 echo "--------------------"
 iptables -S
 echo "--------------------"
-
-sleep 30
 
 exec /bin/bash /etc/jackett/start.sh
